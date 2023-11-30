53ce298738e66b5db247ffb9490cbdcd7e4fc383
Wulgryn <urbanzolika2004@gmail.com>
2023-11-30 03:41:05 +0100

    base, will merge

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "test/test.hpp"
+#include <iostream>
+#include "GLFW/glfw3.h"
+
+int main() {
+    std::cout << "Hello Pandora Engine!" << std::endl;
+    say_hello();
+    return 0;
+}
+
---
+++ src/test/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "test.hpp"
+
+void say_hello()
+{
+    std::cout << "Hello, from test!" << std::endl;
+}
