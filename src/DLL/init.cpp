7d65198fd4000783253d91831f654454afae750e
Wulgryn <urbanzolika2004@gmail.com>
2023-12-02 16:15:03 +0100

    Made glfw init in DLL/init extended fancyLog with logType

--- src/DLL/init.cpp
+++ src/DLL/init.cpp
@@ -1,9 +1,34 @@
 #include "init.hpp"
+
 #include "CONSOLE/fancyLog.hpp"
 
+#include "GLFW/glfw3.h"
 
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+void error_callback(int error, const char* description)
+{
+    logType("GLFW");
+    logError("(%d) -> %s",error,string(description));
+    logType();
+}
 
 void PandoraEngine::init()
 {
-    
+    logInfo("§YELLOWexperimental version 0.0.1 alpha");
+    glfwSetErrorCallback(error_callback);
+    srand(time(NULL));
+
+    if(!glfwInit())
+    {
+        logError("Failed to initialize GLFW!");
+        return;
+    }
+    logSuccess("GLFW initialized successfully!");
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 }
