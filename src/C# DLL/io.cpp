00e0e2728f43e664bdf3d2c47e479ae759063378
Wulgryn <urbanzolika2004@gmail.com>
2023-12-04 00:05:20 +0100

    Update CMakeLists.txt and source files

--- src/C# DLL/io.cpp
+++ src/C# DLL/io.cpp
@@ -1,10 +1,9 @@
-#include "io.hpp"
+#include "io.h"
 #include "IO/io.hpp"
 
 void createFile(const char* path, bool overwrite) {
     io::files::create(path, overwrite);
 }
-
 void createFileWithContent(const char* path, const char* content, bool overwrite) {
     io::files::create(path, content, overwrite);
 }
--- src/DLL/init.cpp
+++ src/DLL/init.cpp
@@ -6,6 +6,7 @@
 
 #include <string>
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
